55624565cc2d662904af449912584a4a37182fbc
Nader Jafari <nader.jafari1988@gmail.com>
2019-08-01 21:22:17 +0430

    replace react app

---
+++ precache-manifest.860fe68e10312e993b1544b7f281514b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baa1b8686536735cd8e8374f50e6cdf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c5b8dde63207b4d3f23",
+    "url": "/static/css/2.22dee1d4.chunk.css"
+  },
+  {
+    "revision": "ecd4a83224cd682eb715",
+    "url": "/static/css/main.ab93973c.chunk.css"
+  },
+  {
+    "revision": "6c5b8dde63207b4d3f23",
+    "url": "/static/js/2.a170f104.chunk.js"
+  },
+  {
+    "revision": "ecd4a83224cd682eb715",
+    "url": "/static/js/main.249ace61.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
